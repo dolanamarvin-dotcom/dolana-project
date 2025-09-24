aca428481d316402b6ac9fa7db25182b853e0232
dolanamarvin-dotcom <dolanamarvin@gmail.com>
2025-09-24 13:55:33 +0800

    login to regestration

--- login.js
+++ login.js
@@ -1,5 +1,14 @@
 function login(){
-    let email = document.getElemetsById("email").value;
+    //Email:dolanamarvin@gmail.com //Error Message:Your emial is Incorect
+    //Password:
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    
 
     console.log("You enter:" + " " + email);
+    console.log("You enter:" + " " + password);
+
+    if (email===""){
+        console.log("Tama ang iyong email");
+    }else
 }
